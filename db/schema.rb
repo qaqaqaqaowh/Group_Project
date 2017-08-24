a5799d224cb2ca915acd1a480772b710cdcbd8a8
qaqaqaqaowh <nicholas@hotmail.com>
2017-08-24 14:47:35 +0800

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20170824034556) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.string "email", null: false
     t.string "encrypted_password", limit: 128, null: false
     t.string "confirmation_token", limit: 128
