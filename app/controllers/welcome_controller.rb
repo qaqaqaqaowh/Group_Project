cf30ef50e18176169c99a30576549225b62a4e5c
Tan Ga Foo <tangafoo@Tans-MacBook-Pro.local>
2017-08-26 00:15:23 +0800

    Moved files to assets

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
 	include Clearance::Controller
  	protect_from_forgery with: :exception
- 	skip_before_action :require_login
 
 	def index
 		if signed_in?
