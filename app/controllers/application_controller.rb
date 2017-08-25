350b206083bf6a53e21f8adaf4421cb7c568c22b
Tan Ga Foo <tangafoo@Tans-MacBook-Pro.local>
2017-08-25 23:57:41 +0800

    1) Finished the front video 2) Added new 'Connect' image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery with: :exception
-  	before_action :require_login
 
 
 	private
@@ -12,5 +11,5 @@ class ApplicationController < ActionController::Base
 	      redirect_to sign_in_path # halts request cycle
 	    end
 	end
-  
+
 end
