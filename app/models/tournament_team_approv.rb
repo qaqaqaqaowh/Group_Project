20636542bda778ed8fb17e55c55de30833ea9f4c
qaqaqaqaowh <nicholas@hotmail.com>
2017-08-29 16:00:38 +0800

    asd

--- app/models/tournament_team_approv.rb
+++ app/models/tournament_team_approv.rb
@@ -1,5 +1,5 @@
 class TournamentTeamApprov < ApplicationRecord
 	belongs_to :tournament
 	belongs_to :team
-	validates_uniqueness_of :team_id, scope: :tournament_id
+	validates_uniqueness_of :team_id, scope: :tournament_id, on: [:new]
 end
